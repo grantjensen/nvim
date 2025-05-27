60b58b369531cd549d51e7c0a995ff49f865f377
Grant Jensen <grantjensen@google.com>
2025-05-27 13:53:53 -0700

    Remove :term optimization
    tmux > nvim:term

--- init.lua
+++ init.lua
@@ -180,9 +180,6 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- Enter terminal mode and run last command
-vim.keymap.set('n', '<A-CR>', 'i!!<CR>', { desc = 'run last terminal command in normal mode' })
-
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
